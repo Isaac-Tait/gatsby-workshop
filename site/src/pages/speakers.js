e170df2b833698ca7c57825d472bd620cf2f0651
Isaac Tait <isaac@mountaintopcoding.com>
2020-10-07 13:02:55 -0700

    my code was WAY off

--- site/src/pages/speakers.js
+++ site/src/pages/speakers.js
@@ -1,11 +1,48 @@
+import { useStaticQuery } from "gatsby";
 import React from "react";
 
-import Speaker from "../components/speakerCards";
+import Layout from "../components/layout";
+import SpeakerCard from "../components/speakerCards";
+import SEO from "../components/seo";
 
-export default function Speakers() {
+const Hero = ({ speakers }) => (
+  <div className="text-center">
+    <h2 className="text-5xl font-extrabold text-blue-500 leading-9 tracking-tight font-inter p-4">
+      Speakers
+    </h2>
+    <SpeakerCard speakers={speakers} />;
+  </div>
+);
+
+function SpeakerPage() {
+  const data = useStaticQuery(graphql`
+    query FetchSpeakerData {
+      allSpeakersYaml {
+        edges {
+          node {
+            id
+            avatar
+            name
+            title
+            time
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
   return (
-    <div>
-      <Speaker />
-    </div>
+    <Layout>
+      <SEO
+        title="Home"
+        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
+      />
+      <Hero speakers={data.allSpeakersYaml.edges} />
+    </Layout>
   );
 }
+
+export default SpeakerPage;
