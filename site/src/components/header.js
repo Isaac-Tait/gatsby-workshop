ed73e749fc7b4b5ca423677380e4c3bee8e75fee
Isaac Tait <isaac@mountaintopcoding.com>
2020-10-07 11:04:50 -0700

    fixed misspelled icon name

--- site/src/components/header.js
+++ site/src/components/header.js
@@ -1,7 +1,8 @@
 import { graphql, Link, useStaticQuery } from "gatsby";
 import PropTypes from "prop-types";
 import React, { useState } from "react";
-import Logo from "../images/snow-mountain.png";
+import Logo from "../images/snow-mountain.svg";
+import Banner from "../components/banner";
 
 function Header({ siteTitle }) {
   const [isExpanded, toggleExpansion] = useState(false);
@@ -24,52 +25,55 @@ function Header({ siteTitle }) {
   const linkOne = data.site.siteMetadata.linkOne;
 
   return (
-    <nav className="bg-white">
-      <div className="flex flex-wrap items-center justify-between max-w-4xl mx-auto p-4 md:p-8">
-        <Link to="/" className="flex items-center no-underline text-black">
-          <img src={Logo} alt="Logo" className="w-8" />
-          <span className="pl-4 text-3xl font-Inter font-extrabold text-black leading-9 tracking-tight">
-            {siteTitle}
-          </span>
-        </Link>
+    <div>
+      <Banner />
+      <nav className="bg-white">
+        <div className="flex flex-wrap items-center justify-between max-w-4xl mx-auto p-4 md:p-8">
+          <Link to="/" className="flex items-center no-underline text-black">
+            <img src={Logo} alt="Logo" className="w-8" />
+            <span className="pl-4 text-3xl font-Inter font-extrabold text-black leading-9 tracking-tight">
+              {siteTitle}
+            </span>
+          </Link>
 
-        <button
-          className="block md:hidden border border-white flex items-center px-3 py-2 rounded text-white"
-          onClick={() => toggleExpansion(!isExpanded)}
-        >
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+          <button
+            className="block md:hidden border border-white flex items-center px-3 py-2 rounded text-white"
+            onClick={() => toggleExpansion(!isExpanded)}
           >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
 
-        <div
-          className={`${
-            isExpanded ? `block` : `hidden`
-          } md:block md:items-center w-full md:w-auto`}
-        >
-          <div className="">
-            {NavLinks &&
-              NavLinks.map((node) => (
-                <Link
-                  key={node.href}
-                  to={node.href}
-                  className="block md:inline-block mt-4 md:mt-0 mr-6 no-underline text-black"
-                >
-                  {node.name}
-                </Link>
-              ))}
-            <ul>
-              <Link to="/ticket">{linkOne}</Link>
-            </ul>
+          <div
+            className={`${
+              isExpanded ? `block` : `hidden`
+            } md:block md:items-center w-full md:w-auto`}
+          >
+            <div className="">
+              {NavLinks &&
+                NavLinks.map((node) => (
+                  <Link
+                    key={node.href}
+                    to={node.href}
+                    className="block md:inline-block mt-4 md:mt-0 mr-6 no-underline text-black"
+                  >
+                    {node.name}
+                  </Link>
+                ))}
+              <ul>
+                <Link to="/ticket">{linkOne}</Link>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
 
