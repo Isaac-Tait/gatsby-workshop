5e19a5950e23b30fea1ab31be188fd588e72bdda
Isaac Tait <isaac@mountaintopcoding.com>
2020-10-07 11:08:44 -0700

    added link to the banner

--- site/src/components/banner.js
+++ site/src/components/banner.js
@@ -1,12 +1,10 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
+
 export default function Banner() {
   return (
     <div className="bg-blue-500 text-white p-4 text-center">
-      <blockquote>
-      
-        Tickets are on sale now for X Conf! 
-       
-      </blockquote>
+      <Link to="/ticket">Tickets are on sale now for X Conf!</Link>
     </div>
   );
 }
